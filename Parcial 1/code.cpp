b3a964bf3723a6fcfcd2749eac6de75f4118196a
Juan Jose Mayorga <jmayorgab@unal.edu.co>
2023-03-06 09:08:41 -0500

    commit

--- Parcial 1/code.cpp
+++ Parcial 1/code.cpp
@@ -1,1 +1,18 @@
 #include <iostream>
+#include <cmath>
+#include <fstream>
+
+double sqrt_heron(double x, int n){
+
+// La función nos retorna la raiz cuadrada de x despues de hacer n iteraciones con la aproximación de heron
+
+    double r = 1;
+
+    for (int i = 1; i <= n; i++) {
+        r = (r + x/r) / 2;
+    }
+    return r;
+
+}
+
+
