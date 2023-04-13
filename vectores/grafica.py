57be8a856b129dbb9e5928ec05fc289c0c057e0a
Juan Jose Mayorga <jmayorgab@unal.edu.co>
2023-04-12 21:28:23 -0500

    commit ejercicio funcion de probabilidad

---
+++ vectores/grafica.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+y = np.linspace(0, 1, 1000)
+
+def g(y):
+    return 30*y*(1-y)**4
+
+def G(y):
+    return np.cumsum(g(y))*(y[1]-y[0])
+
+x, pdf, cdf  = np.loadtxt("stats.txt", unpack=True)
+
+fig, ax = plt.subplots()
+
+ax.plot(x, pdf, color='red', label=r"$y=f(x)$")
+ax.plot(x, cdf, color='blue', label=r"$y=F(x)$")
+ax.plot(y, g(y), color='orange', label=r"$f(x)$ teorica")
+ax.plot(y, G(y), color='c', label=r"$F(x)$ teorica")
+
+ax.set_xlabel(r"\boldmath{$x$}", fontsize=16)
+ax.set_ylabel(r'\boldmath{$y$}', fontsize=16)
+ax.set_title(r'\textbf{Estimación numerica de las funciones pdf} \boldmath{$f(x)$} \textbf{y cdf} \boldmath{$F(x)$} \textbf{de la distribución beta para} \boldmath{$\alpha$} \textbf{= 2.0 y} \boldmath{$\beta$} \textbf{= 5.0}', x=0.5, y=1, fontsize=15)
+
+ax.legend(framealpha=1, shadow=True, fontsize=14, edgecolor="k",borderaxespad=1, borderpad=0.75)
+#ax.set_xscale('log')
+#ax.set_yscale('log')
+ax.grid(True, which='minor', linestyle="-.", alpha=0.5)
+ax.grid(True, linestyle="-", alpha=1)
+ax.minorticks_on()
+ax.tick_params(which="both", width="1.5")
+ax.tick_params(which="minor", length=4, direction="in")
+ax.tick_params(which="major", length=12, direction="inout")
+ax.set_xlim(0, 0.9 )
+ax.set_ylim(0, 2.6)
+#ax.set_xticks([0.000, 0.005, 0.010, 0.015, 0.020, 0.025, 0.030])
+#ax.set_xticklabels(["0", "0.005", "0.010", "0.015", "0.020", "0.025", "0.030"])
+ax.spines['top'].set_linewidth(1.5)
+ax.spines['bottom'].set_linewidth(1.5)
+ax.spines['left'].set_linewidth(1.5)
+ax.spines['right'].set_linewidth(1.5)
+
+plt.show()
+
+fig.savefig("fig_datos.pdf",bbox_inches='tight')
---
+++ vectores/probfunctions.cpp
@@ -0,0 +1,145 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<fstream>
+#include<string>
+#include<algorithm>
+
+void lim_vector(std::vector<double>& v, double& val_min, double& val_max);
+std::vector<double> histograma(std::vector<double>& v, double& val_min, double& val_max, int N);
+std::vector<double> pdf(std::vector<double>& v, int& data_size, double& val_min, double& val_max, int N);
+std::vector<double> cdf(std::vector<double> pdf, double& val_min, double& val_max, int N);
+std::vector<double> x_func(double& val_min, double& val_max, int N);
+
+int main(int argc, char **argv){
+
+    int N = std::atoi(argv[2]);
+
+    std::vector<double> xdata;
+    double x;
+
+    std::string fname = argv[1];
+
+    std::ifstream fin (fname); 
+
+    while (fin >> x) { 
+
+	    xdata.push_back(x); 
+
+    }
+
+    fin.close();
+
+    double min, max;
+    lim_vector(xdata, min,  max);
+    std::vector<double> hist = histograma(xdata, min, max, N);
+
+    int size = xdata.size();
+    std::vector<double> prob_func = pdf(hist, size, min, max, N);
+
+    std::vector<double> cum_func = cdf(prob_func, min, max, N);
+
+    std::vector<double> x_val = x_func(min, max, N);
+
+    std::ofstream fout("stats.txt");
+
+    for(int i = 0; i < N; i++){
+
+        fout << x_val[i] << "\t" << prob_func[i] << "\t" << cum_func[i] << "\n";
+
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+void lim_vector(std::vector<double>& v, double& val_min, double& val_max) {
+
+    val_min = val_max= v[0];
+
+    for (const auto& x : v) {
+
+        if (x < val_min) {
+
+            val_min = x;
+
+        }
+        if (x > val_max) {
+
+            
+            val_max = x;
+        }
+
+    }
+}
+
+std::vector<double> histograma(std::vector<double>& v, double& val_min, double& val_max, int N){
+
+    std::vector<double> hist(N, 0.0);
+
+    double delta_x = (val_max - val_min) / N;;
+
+    for(int i = 0; i < N ; i++){
+
+        for(int j = 0; j < v.size(); j++){
+
+            if(((val_min + (delta_x * i)) <= v[j]) && (v[j] < (val_min + (delta_x * (i + 1))))){
+
+                hist[i]++;
+
+            }            
+        }
+
+    }
+
+    return hist;
+}
+
+std::vector<double> pdf(std::vector<double>& v, int& data_size, double& val_min, double& val_max, int N){
+
+    std::vector<double> pdf(N, 0.0);
+
+    double delta_x = (val_max - val_min) / N;
+
+    for (int i = 0; i < N; i++) {
+
+        pdf[i] = v[i] / (data_size * delta_x);
+
+    }   
+
+return pdf;
+
+}
+
+std::vector<double> cdf(std::vector<double> pdf, double& val_min, double& val_max, int N){
+
+    std::vector<double> cdf(N, 0.0);
+    double delta_x = (val_max - val_min) / N;
+
+    cdf[0] = pdf[0] * delta_x;
+
+    for (int i = 1; i < N; i++) {
+
+        cdf[i] = cdf[i-1] + pdf[i] * delta_x;
+
+    }   
+
+    return cdf;
+
+}
+
+std::vector<double> x_func(double& val_min, double& val_max, int N){
+
+    double delta_x = (val_max - val_min) / N;
+    std::vector<double> x(N, 0.0);
+
+    for (int i = 0; i < N; i++) {
+
+        x[i] = val_min + (i * delta_x);
+
+    }
+
+    return x;
+
+}
